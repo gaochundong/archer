3f57ee9f8d6838295d9aae4f039382972db2b543
Dennis Gao <gaochundong@hotmail.com>
2020-11-06 19:29:56 +0800

    Update brew-install.sh

--- brew/brew-install.sh
+++ brew/brew-install.sh
@@ -4,5 +4,9 @@ https://brew.sh/
 2. List brew install
 brew list | sed s/\s+/\n/g
 brew cask list | sed s/\s+/\n/g
+brew list --cask | sed s/\s+/\n/g
 
 3. brew tap
+
+4. Reinstall List
+brew list | sed s/\s+/\n/g | xargs -L 1 echo 'brew install'
